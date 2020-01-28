60db7921eb8fe444eb100abf72c5a733aab8594b
William Blacknall <williamblacknall@gmail.com>
2020-01-28 12:12:52 -0500

    Setting up Redux, currently creating alert functionality

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -6,20 +6,26 @@ import { Register } from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
 import "./App.css";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
 	return (
-		<Router>
-			<>
-				<Navbar />
-				<Route exact path="/" component={Landing} />
-				<section className="container">
-					<Switch>
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/register" component={Register} />
-					</Switch>
-				</section>
-			</>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<>
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<section className="container">
+						<Switch>
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/register" component={Register} />
+						</Switch>
+					</section>
+				</>
+			</Router>
+		</Provider>
 	);
 };
 
--- devconnector/client/src/components/auth/Login.js
+++ devconnector/client/src/components/auth/Login.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+	const [formData, setFormData] = useState({
+		email: "",
+		password: ""
+	});
+
+	const {email, password} = formData;
+
+	const onChange = e =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async e => {
+		e.preventDefault();
+		console.log("Success");
+		// const newUser = {
+		// 	name,
+		// 	email,
+		// 	password
+		// };
+
+		// try {
+		// 	const config = {
+		// 		headers: {
+		// 			"Content-Type": "application/json"
+		// 		}
+		// 	};
+
+		// 	const body = JSON.stringify(newUser);
+
+		// 	const res = await axios.post("http://localhost:5000/api/users", body, config);
+		// 	console.log(res.data);
+		// } catch (err) {
+		// 	console.error(err.response.data);
+		// }
+	};
+	return (
+		<>
+			<h1 className="large text-primary">Sign In</h1>
+			<p className="lead">
+				<i className="fas fa-user"></i> Login To Your Account
+			</p>
+			<form className="form" onSubmit={e => onSubmit(e)}>
+				<div className="form-group">
+					<input
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={e => onChange(e)}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						minLength="6"
+						value={password}
+						onChange={e => onChange(e)}
+					/>
+				</div>
+				<input type="submit" className="btn btn-primary" value="Login" />
+			</form>
+			<p className="my-1">
+				Don't have an account? <Link to="/register">Sign Up</Link>
+			</p>
+		</>
+	);
+};
