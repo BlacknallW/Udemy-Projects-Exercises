ea9741e0522d923a7070f17cca015038079d7722
William Blacknall <williamblacknall@gmail.com>
2020-08-04 15:17:31 -0400

    Created custom hook and implemented file upload template with error checks. Doesn't currently fully function

---
+++ firegram-starter-files/src/firebase/config.js
@@ -0,0 +1,24 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+import "firebase/analytics"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA1n8nNIfBxtyqUc1zP-_NopFO5fhZtNDw",
+    authDomain: "firegram-65937.firebaseapp.com",
+    databaseURL: "https://firegram-65937.firebaseio.com",
+    projectId: "firegram-65937",
+    storageBucket: "firegram-65937.appspot.com",
+    messagingSenderId: "96278403852",
+    appId: "1:96278403852:web:7776c5c7df7109b5a04729",
+    measurementId: "G-3LLTWRQVBF",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics()
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
