71e0b2d51f0bc7b48ae275426fa59bc7213a6976
William B <williamblacknall@gmail.com>
2020-09-02 11:05:54 -0400

    Added firebase functions...functionality, but can't run due to lacking Blaze plan

--- firegram-starter-files/src/App.js
+++ firegram-starter-files/src/App.js
@@ -9,11 +9,11 @@ function App() {
     const [selectedImg, setSelectedImg] = useState(null);
     const [name, setName] = useState(null);
     const [email, setEmail] = useState(null);
-    const [username, setUsername] = useState(null)
+    const [username, setUsername] = useState(null);
     return (
         <div className="App">
             <Title />
-<UploadForm />
+            <UploadForm />
             <div className="tile is-ancestor">
                 <div className="tile is-4 is-vertical is-parent">
                     <AdminConsole
